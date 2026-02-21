329c5b89e4565b6624e05e0258eff9b77efc28d6
Deepak <Deepakjaiswar678@gmail.com>
2026-02-22 02:59:45 +0530

    First version is fully functional.

---
+++ etl-script.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import psycopg2
+
+data = pd.read_csv('data.csv')
+data['total'] = data['quantity'] * data['price']
+
+try:
+    conn = psycopg2.connect(
+        dbname = input("Enter your database name: "),       # enter the database name eg. -> studentdb
+        user = 'postgres',                                  # enter the database username eg. -> postgres
+        password =input("Enter your database password: "),  # enter the database password
+        host ='localhost',                                  # enter the database host name eg. -> localhost 
+        port ='5432'                                        # enter the database port number eg. -> 5432
+    )
+    print("Database connection successful!")
+
+except Exception as e:
+    print(f'Database connection failed: {e}')
+
+cur = conn.cursor()
+for _, row in data.iterrows():
+    cur.execute("INSERT INTO sales (order_id, customar, product, quantity, price, date, total) values (%s,%s,%s,%s,%s,%s,%s)", (row["order_id"], row["customar"], row["product"], row["quantity"], row["price"], row["date"], row["total"]))
+    conn.commit()
+cur.close()
+conn.close()
